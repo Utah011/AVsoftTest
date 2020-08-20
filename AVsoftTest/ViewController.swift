c1f99ff6eac0b6ee8f9a59a515da097098c1d672
Utah011 <61595568+Utah011@users.noreply.github.com>
2020-08-20 13:00:05 +0300

    initital commit

---
+++ AVsoftTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVsoftTest
+//
+//  Created by Andrew Kolbasov on 18.08.2020.
+//  Copyright © 2020 Andrew Kolbasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
